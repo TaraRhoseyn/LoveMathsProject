6c4c256f3e038ac029692c86a7eac317724a7411
Tara Rhoseyn <taragulwell@gmail.com>
2021-06-07 19:48:55 +0000

    correct mistake in dom content uploaded event handler

--- script.js
+++ script.js
@@ -7,9 +7,8 @@
 // Wait for the DOM to finish loading before running the game - otherwise you could be trying to execute on a page/element that doesn't exist yet.
 // Get the button elements and add event listeners to them
 
-
-document.addEventListener("DOMContentUploaded", function() {
-    let buttons = document.getElementsByTagName('button'); // this returns items as an array
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button"); // this returns items as an array
 
     // the below code does exactly the same as what i've already learnt as: for (let i = 0; i < buttons.length; i++). It iterates through the above array
     // the below syntax is just a modern version of the for/let syntax in the comment above
@@ -19,15 +18,16 @@ document.addEventListener("DOMContentUploaded", function() {
         button.addEventListener("click", function() {
             // below checks if the value of the data-type attribute in 'button' variable items is strictly equal to 'submit'
             if (this.getAttribute("data-type") === "submit") { 
-                alert("You clicked Submit!");
+                alert("You clicked Submit")
             } else {
                 // below tells us what game type attribute we want to run, e.g. all the other buttons, multiply/subtract etc. We're setting the game type (variable) to the value of that HTML data-type attribute value, e.g. "multiply"
                 let gameType = this.getAttribute("data-type");
                 alert(`You clicked ${gameType}`); // when using ${} remember to use the backticks `` and not quotations ''
             }
-        })
+        });
     }
-})
+});
+
 
 
 // Code to be executed when the user clicks a button
